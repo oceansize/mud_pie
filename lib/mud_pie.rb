09f489b371e06e7edb7dd212e1df8cb4e84a479f
Roi Driscoll <roi.driscoll@gmail.com>
2014-06-12 14:27:54 +0100

    configure for cookie implementation

--- lib/mud_pie.rb
+++ lib/mud_pie.rb
@@ -1,7 +1,12 @@
 require 'rubygems'
 require 'sinatra'
+
 set :views, Proc.new { File.join(root, '..', "views") }
 set :public_folder, Proc.new { File.join(root, '..', "public") }
+
+configure do
+  enable :sessions
+end
 
 get '/' do
   erb :index
