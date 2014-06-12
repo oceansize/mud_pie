3fadf3e1f007a7bc1305999bbb9a166729884942
Roi Driscoll <roi.driscoll@gmail.com>
2014-06-12 12:18:18 +0100

    initial commit

---
+++ lib/mud_pie.rb
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'sinatra'
+set :views, Proc.new { File.join(root, '..', "views") }
+set :public_folder, Proc.new { File.join(root, '..', "public") }
+
+get '/' do
+  erb :index
+end
